5fc70b82577acef67f594436e7a7819a592e5939
Getúlio Werle <getuliowerle10@gmail.com>
2025-04-16 17:00:16 -0400

    test: forgot password page

--- tests/Feature/AuthTest.php
+++ tests/Feature/AuthTest.php
@@ -14,3 +14,11 @@ it('display the login page when not logged in', function () {
     expect($this->get('/login')->content())->toContain('Forgot your password?');
 
 });
+
+it('display the recover password page correctly', function () {
+
+    // verify forgot password page URL and content
+    expect($this->get('/forgot-password')->status())->toBe(200);
+    expect($this->get('/forgot-password')->content())->toContain('Remember my password?');
+
+});
